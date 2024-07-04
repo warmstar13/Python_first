084ffc21cf887567323d26aac6f3bbffdcd4d3cb
Aliaksandr <warmstar13@gmail.com>
2024-07-04 19:20:33 +0300

    firstt part of chapter 10 done

--- 20240702IntroduceClasses/chapter10/part1.py
+++ 20240702IntroduceClasses/chapter10/part1.py
@@ -4,4 +4,20 @@ curpath = Path(__file__).parent
 path = curpath / 'new_text.txt'
 text = path.read_text()
 print(text)
-print(type(text))
+print(type(path))
+list = text.splitlines()
+string = ''
+for str in list:
+    str = str.strip()
+    string += str
+print(string)
+
+for x in string:
+    print(x)
+
+# x = "123456"
+# print(x[1:3])
+
+# for x in range(1, 5):
+#     x -= 1
+#     print(x)
