b2455a0d28eaa76b9e0d12d84ef718297270bc1f
Aliaksandr <warmstar13@gmail.com>
2024-07-11 17:56:58 +0300

    Made some optimization

--- PythonCrashCourse/Alien_Invasion/alien_invasion.py
+++ PythonCrashCourse/Alien_Invasion/alien_invasion.py
@@ -17,15 +17,23 @@ class AlienInvasion:
     def run_game(self):
         """Start the main loop for the game."""
         while True:
-         # Watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-                self.screen.fill(self.settings.bg_color)
-                # Make the most recently drawn screen visible.
-                self.ship.blitme()
-                pygame.display.flip()
-                self.clock.tick(60)
+            self._check_events()
+            self._update_screen()
+            self.clock.tick(60)
+
+    def _check_events(self):
+     # Watch for keyboard and mouse events.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+    def _update_screen(self):
+        # Make the most recently drawn screen visible.
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        pygame.display.flip()
+
+        
 if __name__ == '__main__':
     # Make a game instance, and run the game.
     ai = AlienInvasion()
