4606b80a0dd09e1cd5f476ed2630bccad928a824
Aliaksandr <warmstar13@gmail.com>
2024-07-13 20:16:08 +0300

    added boundaries to the ship movement

--- PythonCrashCourse/Alien_Invasion/settings.py
+++ PythonCrashCourse/Alien_Invasion/settings.py
@@ -5,3 +5,6 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (50, 50, 150)
+
+        # ship settings
+        self.ship_speed = 2
--- PythonCrashCourse/Alien_Invasion/ship.py
+++ PythonCrashCourse/Alien_Invasion/ship.py
@@ -6,13 +6,16 @@ class Ship:
 
     def __init__(self, ai_game):
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.rect_screen = ai_game.screen.get_rect()
+        
         self.path = Path(__file__).parent / "images/hand-drawn-spaceship-background.bmp"
         self.image = pygame.image.load(self.path)
         self.image = pygame.transform.scale(self.image, (180, 200))
+        
         self.rect = self.image.get_rect()
         self.rect.midbottom = self.rect_screen.midbottom
-
+        self.x = float(self.rect.x)
         # Setting the moving of the ship
 
         self.move_right = False
@@ -22,7 +25,8 @@ class Ship:
         self.screen.blit(self.image, self.rect)
 
     def update(self):
-        if self.move_left:
-            self.rect.x -= 1
-        elif self.move_right:
-            self.rect.x += 1
+        if self.move_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+        elif self.move_right and self.rect.right < self.rect_screen.right:
+            self.x += self.settings.ship_speed
+        self.rect.x = self.x
