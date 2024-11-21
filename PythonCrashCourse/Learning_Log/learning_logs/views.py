d16de5ef2ee81dfbffea85c50ad0bdf2dd31df29
Aliaksandr <warmstar13@gmail.com>
2024-11-21 21:51:02 +0300

    Chapter 19 almost done

--- PythonCrashCourse/Learning_Log/learning_logs/views.py
+++ PythonCrashCourse/Learning_Log/learning_logs/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
+from .__init__ import check_topic_owner
 
 # Create your views here.
 
@@ -9,32 +12,40 @@ def index(request):
     # The home page for learning log.
     return render(request, 'learning_logs/index.html')
 
+@login_required
 def topics(request):
     # Show topic
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     context = {"topics": topics}
     return render(request, 'learning_logs/topics.html', context)
 
+@login_required
 def topic(request, topic_id):
     topic = Topic.objects.get(id=topic_id)
+    check_topic_owner(request, topic)
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
 
+@login_required
 def new_topic(request):
     if request.method != 'POST':
         form = TopicForm()
     else:
         form = TopicForm(data=request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return redirect('learning_logs:topics')
         
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
 
+@login_required
 def new_entry(request, topic_id):
     topic = Topic.objects.get(id=topic_id)
+    check_topic_owner(request, topic)
 
     if request.method != 'POST':
         form = EntryForm()
@@ -49,10 +60,12 @@ def new_entry(request, topic_id):
     context = {'topic': topic, 'form': form}
     return render(request, 'learning_logs/new_entry.html', context)
 
+@login_required
 def edit_entry(request, entry_id):
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
-    
+    check_topic_owner(request, topic)
+
     if request.method != 'POST':
         form = EntryForm(instance=entry)
     else:
--- PythonCrashCourse/Learning_Log/ll_project/settings.py
+++ PythonCrashCourse/Learning_Log/ll_project/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     # My apps 
     'learning_logs',
+    'accounts',
     # default apps
     'django.contrib.admin',
     'django.contrib.auth',
@@ -124,3 +125,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# My settings.
+LOGIN_REDIRECT_URL = 'learning_logs:index'
+LOGOUT_REDIRECT_URL = 'learning_logs:index'
+LOGIN_URL = 'accounts:login'
