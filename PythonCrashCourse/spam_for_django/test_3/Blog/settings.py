58f80699bcc1549ede8402138f083e999c0ae094
Aliaksandr <warmstar13@gmail.com>
2024-11-21 20:45:25 +0300

    Chapter 19 hw 2 done

--- PythonCrashCourse/spam_for_django/test_3/Blog/settings.py
+++ PythonCrashCourse/spam_for_django/test_3/Blog/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'blogs',
+    'accounts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -122,3 +123,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'blogs:mainpage'
+LOGOUT_REDIRECT_URL = 'blogs:mainpage'
